1bd5ec002d85c84e1449e1041370950a0ab18c2e
Cristian Pallarés <skyrpex@gmail.com>
2012-10-10 13:12:05 +0200

    Changing GraphicsJoint style

--- widgets/graphics/GraphicsJoint.cpp
+++ widgets/graphics/GraphicsJoint.cpp
@@ -1,19 +1,57 @@
 #include "GraphicsJoint.h"
 #include "model/Joint.h"
+#include "model/Transform.h"
 #include <QStack>
 #include <QGraphicsScene>
+#include <QGraphicsLineItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsSceneMouseEvent>
 
-static const QRectF JointRect(-5, -5, 10, 10);
+static const QColor RootJointColor(240, 170, 255);
+static const QColor JointColor(170, 240, 255);
+static const QRectF JointRect(-8, -8, 16, 16);
+static const QBrush JointBrush(Qt::white);
+static const QPen   JointPen(JointColor, 5);
+
+static const QPen LinePen(JointColor, 0);
+
+static const int LabelHorizontalOffset = 1;
+static const int LabelVerticalOffset = 10;
 
 GraphicsJoint::GraphicsJoint(Joint *joint, GraphicsJoint *parent) :
     QGraphicsRectItem(JointRect),
     m_joint(joint),
+    m_transform(NULL),
     m_parent(parent),
-    m_line(new QGraphicsLineItem)
+    m_line(new QGraphicsLineItem),
+    m_label(new QGraphicsTextItem(this))
 {
-    setFlags(ItemIsSelectable | ItemIsMovable | ItemSendsScenePositionChanges);
+    setBrush(JointBrush);
+    setPen(JointPen);
+    setFlags(ItemIsSelectable | ItemIsMovable | ItemSendsScenePositionChanges | ItemIgnoresTransformations);
     foreach(Joint *child, joint->children())
         m_children.append(new GraphicsJoint(child, this));
+
+    m_line->setPen(LinePen);
+    m_line->setZValue(-1);
+    m_label->setPos(LabelHorizontalOffset, LabelVerticalOffset);
+
+    setEnabled(false);
+}
+
+void GraphicsJoint::setTransform(Transform *transform)
+{
+    m_transform = transform;
+}
+
+Transform *GraphicsJoint::transform() const
+{
+    return m_transform;
+}
+
+QGraphicsTextItem *GraphicsJoint::label() const
+{
+    return m_label;
 }
 
 QVariant GraphicsJoint::itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value)
@@ -43,4 +81,16 @@ QVariant GraphicsJoint::itemChange(QGraphicsItem::GraphicsItemChange change, con
         }
     }
     return QGraphicsRectItem::itemChange(change, value);
+}
+
+void GraphicsJoint::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    Q_ASSERT(m_transform);
+    QGraphicsRectItem::mouseMoveEvent(event);
+    if(m_parent)
+    {
+        QLineF line(m_parent->pos(), pos());
+        m_transform->length = line.length();
+        m_transform->rotation = line.angle();
+    }
 }
--- widgets/graphics/GraphicsJoint.h
+++ widgets/graphics/GraphicsJoint.h
@@ -2,24 +2,36 @@
 #define GRAPHICSJOINT_H
 
 #include <QGraphicsRectItem>
-#include <QGraphicsLineItem>
 #include <QList>
 
 class Joint;
+class QGraphicsLineItem;
+class QGraphicsTextItem;
+class Transform;
 
 class GraphicsJoint : public QGraphicsRectItem
 {
 public:
     GraphicsJoint(Joint *joint, GraphicsJoint *parent = NULL);
 
+    void setTransform(Transform *transform);
+    Transform *transform() const;
+
+    QGraphicsTextItem *label() const;
+
     QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+
+protected:
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
 
 private:
     Joint *m_joint;
+    Transform *m_transform;
 
     GraphicsJoint *m_parent;
     QList<GraphicsJoint *> m_children;
     QGraphicsLineItem *m_line;
+    QGraphicsTextItem *m_label;
 };
 
 #endif // GRAPHICSJOINT_H
